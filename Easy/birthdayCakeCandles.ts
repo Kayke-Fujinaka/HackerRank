9d3b08960a58ca08520a64e4c9040e613eee836d
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-11-27 16:25:17 -0300

    feat: add validation in easy/birthdayCakeCandles

--- Easy/birthdayCakeCandles.ts
+++ Easy/birthdayCakeCandles.ts
@@ -1,14 +1,18 @@
 const candles = [1, 3, 3, 3];
 
 function birthdayCakeCandles(candles) {
+  const CANDLES_REQUIRED = candles.length <= 0;
+
+  if (CANDLES_REQUIRED) throw new Error("Insert an array with values");
+
   const maxValue = Math.max(...candles);
 
-  const totalCandles = candles.reduce(
+  const totalTallestCandles = candles.reduce(
     (acc, cur) => (cur === maxValue ? [...acc, cur] : acc),
     []
   );
 
-  return totalCandles.length;
+  return totalTallestCandles.length;
 }
 
 console.log(birthdayCakeCandles(candles));
