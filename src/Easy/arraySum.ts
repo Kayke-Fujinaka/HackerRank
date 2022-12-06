af4b21fb800622a86d65e87f32ded93de3c55f67
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-12-06 20:54:53 -0300

    feat: type arraySum exercise

--- src/Easy/arraySum.ts
+++ src/Easy/arraySum.ts
@@ -1,5 +1,6 @@
 const numbers = [1, 2, 3];
 
-const arraySum = (arr: number[]) => arr.reduce((acc, cur) => acc + cur, 0);
+const arraySum = (arr: number[]): number =>
+  arr.reduce((acc, cur) => acc + cur, 0);
 
 console.log(arraySum(numbers));
