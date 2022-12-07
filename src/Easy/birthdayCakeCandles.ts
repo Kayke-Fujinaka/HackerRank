aee23ad1c052256edd8717a437e6645c917970ce
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-12-06 21:15:45 -0300

    feat: type birthdayCake exercise

--- src/Easy/birthdayCakeCandles.ts
+++ src/Easy/birthdayCakeCandles.ts
@@ -1,13 +1,14 @@
 const candles = [1, 3, 3, 3];
 
-function birthdayCakeCandles(candles) {
+function birthdayCakeCandles(candles: number[]): number {
   const CANDLES_REQUIRED = candles.length <= 0;
 
   if (CANDLES_REQUIRED) throw new Error("Insert an array with values");
 
   const maxValue = Math.max(...candles);
 
-  const totalTallestCandles = candles.reduce(
+  // I need use to explicit argument for reduce, ie, i apply <number[]>
+  const totalTallestCandles = candles.reduce<number[]>(
     (acc, cur) => (cur === maxValue ? [...acc, cur] : acc),
     []
   );
