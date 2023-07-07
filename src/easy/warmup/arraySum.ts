2be8fc2e04118637fd4a05e3b4f47a5b7cf261fa
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-07-07 11:18:25 -0300

    chore: format

--- src/easy/warmup/arraySum.ts
+++ src/easy/warmup/arraySum.ts
@@ -2,9 +2,9 @@
 // Input: 1, 2, 3
 // Output: 6
 
-const numbers = [1, 2, 3];
+const numbers = [1, 2, 3]
 
 const arraySum = (arr: number[]): number =>
-  arr.reduce((acc, cur) => acc + cur, 0);
+  arr.reduce((acc, cur) => acc + cur, 0)
 
-console.log(arraySum(numbers));
+console.log(arraySum(numbers))
--- src/easy/warmup/diagonalDifference.ts
+++ src/easy/warmup/diagonalDifference.ts
@@ -10,20 +10,20 @@ const matriz = [
   [1, 2, 10],
   [4, 5, 6],
   [10, 8, 9],
-];
+]
 
 function diagonalDifference(arr: number[][]): number {
   const primaryDiagonal = arr.reduce(
     (acc, value, idx) => (acc += value[idx]),
-    0
-  );
+    0,
+  )
 
   const secondaryDiagonal = arr.reduce(
     (acc, value, idx) => (acc += value.reverse()[idx]),
-    0
-  );
+    0,
+  )
 
-  return Math.abs(primaryDiagonal - secondaryDiagonal);
+  return Math.abs(primaryDiagonal - secondaryDiagonal)
 }
 
-console.log(diagonalDifference(matriz));
+console.log(diagonalDifference(matriz))
